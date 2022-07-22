04bfece48bf58672b706455648da7fe0e5ded815
Joshua Harris <jharrisjoshua@gmail.com>
2022-07-22 00:11:45 -0400

    Refactor functions
    Break up long functions

--- blueprints/calc.py
+++ blueprints/calc.py
@@ -13,7 +13,6 @@ def calc():
 
     if request.method in ["POST", "GET"]:
         payment_info = pmt_calc_route(request, rates)
-
         return render_template("calc.j2", pmt_info=payment_info)
 
 
@@ -25,6 +24,37 @@ def amortization():
     if request.method == "POST":
         payment_info = pmt_calc_route(request, rates)
         return render_template("amortization.j2", pmt_info=payment_info)
+
+
+def pmt_calc_route(re, rates):
+    """Generate Mortgage Payment(pmt) info depending on route"""
+    pmt_info = create_pmt_dict() if request.method == "GET" \
+        else get_post_info(re)
+    pmt_info = pmt_calc(pmt_info, rates)
+    return pmt_info
+
+
+def create_pmt_dict():
+    """Helper method. Generates a dictionary to store payment info"""
+    pmt_info = {"loan_type": "purchase", "loan_term": "30",
+                "home_price": 250000, "down_pmt": 20, "monthly_pmt": 0,
+                "extra_pmt": 0, "rate": 0, "total_principal": 0.0,
+                "total_interest": 0, "sum_pmts": 0}
+
+    return pmt_info
+
+
+def get_post_info(re):
+    """Helper Method. Retrieves info from HTML form."""
+    pmt_info = create_pmt_dict()
+
+    if re.form.get("calc_info"):
+        pmt_info['loan_type'] = re.form["loan_type"]
+        pmt_info['loan_term'] = re.form["loan_term"]
+        pmt_info['home_price'] = int(re.form["home_price"])
+        pmt_info['down_pmt'] = int(re.form["down_pmt"])
+        pmt_info['extra_pmt'] = int(re.form["extra_pmt"])
+    return pmt_info
 
 
 def pmt_calc(pmt_info, rates):
@@ -39,74 +69,52 @@ def pmt_calc(pmt_info, rates):
     return pmt_info
 
 
-def pmt_calc_route(re, rates):
-    """Generate Mortgage Payment(pmt) info depending on route"""
-    pmt_info = create_pmt_dict() if request.method == "GET" \
-        else get_post_info(re)
-    pmt_info = pmt_calc(pmt_info, rates)
-    return pmt_info
-
-
-def get_post_info(re):
-    """Helper Method. Retrieves info from HTML form."""
-    pmt_info = create_pmt_dict()
-
-    if re.form.get("calc_info"):
-        pmt_info['loan_type'] = re.form["loan_type"]
-        pmt_info['loan_term'] = re.form["loan_term"]
-        pmt_info['home_price'] = int(re.form["home_price"])
-        pmt_info['down_pmt'] = int(re.form["down_pmt"])
-        pmt_info['extra_pmt'] = int(re.form["extra_pmt"])
-
-    return pmt_info
-
-
 def pmt_info_helper(info, rate, loan_term):
     """Helper Method. Monthly Payment Calculation. """
     loan_amt = (info['home_price'] * (1 - info['down_pmt']/100))
     payment = round(loan_amt * (rate / 12) /
                     (1 - (1 + rate / 12) ** (-1 * int(loan_term) * 12)), 2)
+
     sum_pmts = int(loan_term) * 12 * payment
     total_interest = sum_pmts - loan_amt
     return payment, sum_pmts, loan_amt, total_interest
 
 
-def create_pmt_dict():
-    """Helper method. Generates a dictionary to store payment info"""
-    pmt_info = {"loan_type": "purchase", "loan_term": "30",
-                "home_price": 250000, "down_pmt": 20, "monthly_pmt": 0,
-                "extra_pmt": 0, "rate": 0, "total_principal": 0.0,
-                "total_interest": 0, "sum_pmts": 0}
-    return pmt_info
+def payment_number_info(values):
+    """Formats info for each payment for amortization schedule dictionary"""
+    headings = ['principal', 'additional', 'interest', 'balance']
+    return dict(zip(headings, values))
 
 
 def amortization_calc(info):
-    """Generate information for amortization schedule"""
-    loan_term = int(info['loan_term'])
-    time = 0
-    balance = info['total_principal']
-    payment = info['monthly_pmt']
-    principal = 0
-    additional = 0
-    interest = 0
-    total_interest = 0
-
-    amort_info = {'schedule': {}}
-    headings = ['principal', 'additional', 'interest', 'balance']
-    values = [principal, additional, interest, balance]
-    amort_info['schedule'][time] = dict(zip(headings, values))
-
-    while balance > 0:
-        time += 1
-        interest = round(balance * info['rate'] / (12 * 100), 2)
-        total_interest += interest
-        additional = info['extra_pmt']
-        principal = round(((interest + balance) if time == (loan_term * 12)
-                     else min(interest + balance, payment - interest)), 2)
-        balance = round(max(0, balance - principal - additional), 2)
-        values = [principal, additional, interest, balance]
-        amort_info['schedule'][time] = dict(zip(headings, values))
-
+    """Generate dictionary with amortization schedule info"""
+    amort_info, values = {'schedule': {}}, [0, 0, 0, info['total_principal']]
+    amort_info, total_interest = amortization_schedule(values, amort_info, info)
     amort_info['total_interest'] = round(total_interest, 0)
-    amort_info['tot_pmt'] = round(total_interest + info['total_principal'], 0)
+    amort_info['total_pmt'] = round(total_interest + info['total_principal'], 0)
     return amort_info
+
+
+def amortization_schedule(values, amort_info, info, total_interest=0):
+    """Helper Method. Iterates through payment schedule until balance is zero"""
+    principal, extra, interest, balance = values
+
+    time = 0
+    while balance > 0 or interest > 0:
+        values = [principal, extra, interest, balance]
+        amort_info['schedule'][time] = payment_number_info(values)
+        principal, extra, interest, balance = calc_values(values, info, time+1)
+        time, total_interest = time + 1, total_interest + interest
+    return amort_info, total_interest
+
+
+def calc_values(values, info, time):
+    """Helper. Update principal, additional payment, interest, balance values"""
+    principal, extra, interest, balance = values
+    loan_term, payment = int(info['loan_term']), info['monthly_pmt']
+    interest = round(balance * info['rate'] / (12 * 100), 2)
+    extra = info['extra_pmt']
+    principal = round(min(interest + balance, payment - interest)
+                      if time != (loan_term * 12) else (interest + balance), 2)
+    balance = round(max(0, balance - principal - extra), 2)
+    return principal, extra, interest, balance
